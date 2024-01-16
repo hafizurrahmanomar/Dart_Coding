88d91f25dba41d7fd142e8949bb41d596d3990df
hafizurrahmanomar <hafiztechacademy@gmail.com>
2024-01-17 00:51:58 +0600

    Initial commit

---
+++ 3.0_list.dart
@@ -0,0 +1,16 @@
+import 'dart:html';
+import 'dart:html_common';
+
+void main() {
+
+  
+  //String  list
+  List<String> name = ["Hafizur", "Rahman", "Omar"];
+  print(name);
+  print(name[1]);
+  //
+  List<int> age = [25,30,50];
+  print(age);
+
+  } 
+
